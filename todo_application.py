845949dd50fbf3bbd083e3a4680d2f92a5e2c07f
Pathan Sharukh Khan <145139884+sharukh010@users.noreply.github.com>
2026-05-31 06:55:15 +0530

    added steps to upload files into github

--- todo_application.py
+++ todo_application.py
@@ -1,1 +1,25 @@
-print("hello, world")
+"""
+Steps to upload the files for the first time in github: 
+1. Go to github.com 
+2. Login to your account 
+3. click on your profile on the right top corner 
+4. choose repository option 
+5. click on new 
+6. provide repository name 
+7. provide description 
+8. click on upload existing files ( this will take you to the file explorer)
+9. select the files you want to upload and then click open 
+10. give a commit message " added fileName " 
+11. click on commit 
+
+Steps to upload the files after creating the repository: 
+1. Go to github.com 
+2. Login to your account 
+3. click on your profile on the right top corner 
+4. choose repository option 
+5. choose your repository Python Level 1 
+6. Click on the + button to upload the files 
+7. select the files you want to upload and then click open 
+8. give a commit message " added fileName " 
+9. click on commit 
+"""
